74a98cf85027b6a182498d4f13ea6df82544245f
Rishon Dsouza <rishonv.dsouza@gmail.com>
2024-05-30 16:25:11 +0400

    Removed redir URI

--- src/app/signInButton.tsx
+++ src/app/signInButton.tsx
@@ -7,11 +7,7 @@ import { NextRouter, useRouter } from "next/router";
 // Sign In func
 function GetUserAuth() {
   const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-  const redirectUri = "http://localhost:3000/api/github-callback";
-
-  const authorizationUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
-    redirectUri
-  )}&scope=user,repo`;
+  const authorizationUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user,repo`;
   window.location.href = authorizationUrl;
 }
 
